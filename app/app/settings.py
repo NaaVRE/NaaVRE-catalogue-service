279b18017a7659a3c2cdd5f4ee1bb6e604123ec8
Gabriel Pelouze <gabriel@pelouze.net>
2024-10-21 10:24:10 +0200

    add base path support

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+BASE_PATH = os.environ.get('BASE_PATH', '').strip('/')
+if BASE_PATH:
+    STATIC_URL = f'{BASE_PATH}/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from app.settings import BASE_PATH
 from virtual_labs.urls import router as virtual_labs_router
 from workflow_cells.urls import router as workflow_cells_router
 from workflows.urls import router as workflows_router
@@ -16,3 +18,9 @@ urlpatterns = [
     path('api-auth/', include("rest_framework.urls", namespace="rest_framework")),
     path('', include(router.urls)),
     ]
+
+if BASE_PATH:
+    urlpatterns = [
+        path(f'{BASE_PATH}/', include(urlpatterns)),
+        path('', lambda request: redirect(f'{BASE_PATH}/')),
+        ]
