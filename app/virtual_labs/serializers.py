36c42958fd5859ab77b292c411a0283f93683b59
Gabriel Pelouze <gabriel@pelouze.net>
2025-09-04 09:44:36 +0200

    add virtual lab labels

--- app/virtual_labs/serializers.py
+++ app/virtual_labs/serializers.py
@@ -3,8 +3,26 @@ from rest_framework import serializers
 from . import models
 
 
+class VirtualLabLabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.VirtualLabLabel
+        fields = ['title', 'color']
+
+
 class VirtualLabSerializer(serializers.HyperlinkedModelSerializer):
+    labels = VirtualLabLabelSerializer(many=True, required=False)
+
     class Meta:
         model = models.VirtualLab
-        fields = ['url', 'slug', 'title', 'description', 'created', 'modified',
-                  'deployment_url', 'container_image', 'image']
+        fields = [
+            'url',
+            'slug',
+            'title',
+            'labels',
+            'description',
+            'created',
+            'modified',
+            'deployment_url',
+            'container_image',
+            'image',
+            ]
