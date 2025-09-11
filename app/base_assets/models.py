1aa6e45777f38777f25b90c6aaef47f880ae43ab
Gabriel Pelouze <gabriel@pelouze.net>
2025-09-11 11:12:45 +0200

    CS1 add shared_with_scopes and shared_with_users fields to BaseAsset

--- app/base_assets/models.py
+++ app/base_assets/models.py
@@ -6,6 +6,19 @@ from django.db import models
 from virtual_labs.models import VirtualLab
 
 
+class SharingScope(models.Model):
+    slug = models.SlugField(primary_key=True, unique=True)
+    title = models.CharField(max_length=255)
+    LABEL_CHOICES = {
+        'virtual_lab': 'Virtual Lab',
+        'community': 'Community',
+        }
+    label = models.CharField(max_length=100, choices=LABEL_CHOICES)
+
+    def __str__(self):
+        return f'{self.title} ({self.label})'
+
+
 class VersioningMixin(models.Model):
     version = models.IntegerField(default=1)
     next_version = models.ForeignKey(
@@ -30,6 +43,8 @@ class BaseAsset(models.Model):
         VirtualLab, related_name='assets', null=True,
         on_delete=models.SET_NULL,
         )
+    shared_with_scopes = models.ManyToManyField(SharingScope, blank=True)
+    shared_with_users = models.ManyToManyField(User, blank=True)
 
     class Meta:
         ordering = ['created']
