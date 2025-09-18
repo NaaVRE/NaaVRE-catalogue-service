327bbac2d20488133149d8ccc6e311a41445d9a1
Gabriel Pelouze <gabriel@pelouze.net>
2025-09-18 23:52:42 +0200

    add shared_with_ fields to BaseAssetSerializer

--- app/base_assets/serializers.py
+++ app/base_assets/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from . import models
@@ -12,6 +13,17 @@ class SharingScopeSerializer(serializers.HyperlinkedModelSerializer):
 class BaseAssetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     virtual_lab = serializers.ReadOnlyField(source='virtual_lab.slug')
+    shared_with_scopes = serializers.PrimaryKeyRelatedField(
+        many=True,
+        required=False,
+        queryset=models.SharingScope.objects.all(),
+        )
+    shared_with_users = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        slug_field='username',
+        queryset=User.objects.all(),
+        )
 
     class Meta:
         fields = '__all__'
